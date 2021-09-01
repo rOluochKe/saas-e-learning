64896f3fd49a95609456cf971b9d7380dcec3b1d
roluochKe <rasholuoch@gmail.com>
2021-09-01 15:00:53 +0300

    adding public activity to lessons, actions text and breadcrumbs

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,6 +3,16 @@ class Lesson < ApplicationRecord
 
   validates :title, :content, :course, presence: true
 
+  has_rich_text :content
+
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  include PublicActivity::Model
+
+  tracked owner: proc { |controller, _model| controller.current_user }
+
+  def to_s
+    title
+  end
 end
